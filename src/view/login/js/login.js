38482e5cf7315945d5fe23b22ca0d5bc1ecb3a9f
Bruno Ferreira <brunoferreira1999@live.com>
2021-09-03 08:46:43 -0300

    Adição do javascript para login

--- src/view/login/js/login.js
+++ src/view/login/js/login.js
@@ -1,15 +1,34 @@
 const email = document.getElementById('email');
-const password = document.getElementById('senha');
+const password = document.getElementById('password');
 const btnLogin = document.getElementById('btnLogin');
 
-const corsDef = {
-   method: "GET",
-   mode: "cors",
-   cache: "default",
-};
-
 btnLogin.addEventListener('click', async() => {
-   await fetch(`http://localhost:3000/login/${email.value}/${password.value}`)
-   .then(res => (console.log(res)))
 
-})
+   const parameters = new URLSearchParams();
+   //parameters.append('nome', 'Bruno');
+   parameters.append('email', email.value);
+   parameters.append('senha', password.value);
+
+   const requestDetails = { 
+      method: 'POST',
+      headers: new Headers({
+          'Accept': 'application/json'
+      }),
+      body: parameters
+   }
+
+   let tryLogin = await fetch(`http://localhost:3000/auth/login`, requestDetails);
+   let result = await tryLogin.json();
+
+   if (!result.success)
+      return window.alert(result.error)
+
+      window.alert(result.success);
+      window.location.replace('https://livrados.vercel.app');
+
+   //.then(res => res.json())
+   //.then(data => { console.log(data)});
+
+});
+
+
